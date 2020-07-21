fd6103cbf448a8cf8d628efca2322b5081f1da59
Andrew Johnson <andrewjohnson@Andrews-MacBook-Pro.local>
2020-07-21 14:15:31 -0700

    what happens when a punct is constructed without a span

--- src/lib.rs
+++ src/lib.rs
@@ -34,9 +34,11 @@ pub fn quote_precise(input: proc_macro::TokenStream) -> proc_macro::TokenStream 
    let ts = copy_paste(input);
 
    let output = quote! {
-      let mut s = proc_macro2::TokenStream::new();
-      #ts
-      s
+      {
+         let mut s = proc_macro2::TokenStream::new();
+         #ts
+         s
+      }
    };
 
    proc_macro::TokenStream::from(output)
